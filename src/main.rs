9f7115a8edef60ab3376931cee9b28787e2de8a5
yonchicy <1079279184@qq.com>
2024-10-30 19:19:37 +0800

    add egui frame

--- src/main.rs
+++ src/main.rs
@@ -1,54 +1,38 @@
-use winvd::{get_desktop_count, listen_desktop_events, switch_desktop, DesktopEvent};
+use chrono::Local;
+use eframe::egui;
+use egui::viewport::{self, ViewportBuilder};
 
-fn get_current_desktop_id()->Option<u32>{
-    get_desktop_id(winvd::get_current_desktop().unwrap())
-}
-fn get_desktop_id(desktop: winvd::Desktop)->Option<u32>{
-    for (idx, d) in winvd::get_desktops().unwrap().iter().enumerate(){
-        if *d==desktop{
-            return Some(idx as u32);
-        }
-    }
-    None
-}
 fn main() {
-    // Desktop count
-    println!("Desktops: {:?}", get_desktop_count().unwrap());
-    // get current desktop
-    println!("Current Desktop: {:?}", winvd::get_current_desktop().unwrap());
-    // get current desktop index
+    let native_options = eframe::NativeOptions {
+        viewport: ViewportBuilder::default().with_decorations(true),
+        ..Default::default()
+    };
+    eframe::run_native(
+        "my_app",
+        native_options,
+        Box::new(|cc| Ok(Box::new(MyEguiApp::new(cc)))),
+    );
+}
 
-    // Go to second desktop, index = 1
-    switch_desktop(1).unwrap();
+#[derive(Default)]
+struct MyEguiApp {}
 
-    // To listen for changes, use crossbeam, mpsc or winit proxy as a sender
-    let (tx, rx) = std::sync::mpsc::channel::<DesktopEvent>();
-    let _notifications_thread = listen_desktop_events(tx);
+impl MyEguiApp {
+    fn new(cc: &eframe::CreationContext<'_>) -> Self {
+        // Customize egui here with cc.egui_ctx.set_fonts and cc.egui_ctx.set_visuals.
+        // Restore app state using cc.storage (requires the "persistence" feature).
+        // Use the cc.gl (a glow::Context) to create graphics shaders and buffers that you can use
+        // for e.g. egui::PaintCallback.
+        Self::default()
+    }
+}
 
-    // Keep the _notifications_thread alive for as long as you wish to listen changes
-    std::thread::spawn(|| {
-        for item in rx {
-            println!("get info");
-            println!("{:?}", item);
-            // println current desktop id
-            println!("Current Desktop: {:?}", get_current_desktop_id().unwrap());
-            match item {
-                DesktopEvent::DesktopChanged { new, old } =>{
-                    println!("Desktop changed from {:?} to {:?}", old, new);
-                    // print new desktop name
-                    println!("Desktop changed from {:?} to {:?}", new.get_name().unwrap(), old.get_name().unwrap());
-                    // println old desktop id to new desktop id
-                    println!("Desktop changed from {:?} to {:?}", get_desktop_id(old).unwrap(), get_desktop_id(new).unwrap());
-                }
-                _=>{}
-            }
-        }
-    });
-
-    // Wait for keypress
-    
-    switch_desktop(0).unwrap();
-    println!("⛔ Press enter to stop");
-    let mut input = String::new();
-    std::io::stdin().read_line(&mut input).unwrap();
+impl eframe::App for MyEguiApp {
+    fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
+        let fmt = "%Y:%M:%D %H:%M:%S";
+        let now = Local::now().format(fmt);
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.heading(now.to_string());
+        });
+    }
 }
